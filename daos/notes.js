787e387d859510e5bced2349aec4e93792a4a1d7
Andrew Weiss <wandrew8@gmail.com>
2020-08-02 14:11:44 -0700

    notes routes added and working

--- daos/notes.js
+++ daos/notes.js
@@ -1,4 +1,25 @@
 const Note = require('../models/note');
-
+const Token = require('../models/token');
+const User = require('../models/user');
 module.exports = {};
 
+
+module.exports.findToken = (token) => {
+    return Token.findOne({ token: token }).lean();
+}
+
+module.exports.findUser = (userId) => {
+    return User.findOne({ _id: userId }).lean();;
+}
+
+module.exports.addNote = (text, userId) => {
+    return Note.create({ text: text, userId: userId });
+}
+
+module.exports.getNotes = (userId) => {
+    return Note.find({ userId: userId }).lean();
+}
+
+module.exports.getSingleNote = (noteId) => {
+    return Note.findOne({ _id: noteId }).lean();
+}
--- routes/notes.js
+++ routes/notes.js
@@ -4,32 +4,67 @@ const router = Router();
 const noteDAO = require("../daos/notes");
 
 
-//All of these routes require authentication
-//This middleware should verify that the user is authenticated
+//Middleware function to authenticate all /notes routes
 router.use(async (req, res, next) => {
-    console.log(`${req.method} ${req.path}`);
-    const bearerToken = req.headers.authorization;
+    console.log(`${req.method} notes${req.path}`);
+    const bearerToken = req.headers.authorization.split(" ")[1];
     const isAuthenticated = await noteDAO.findToken(bearerToken);
+    if (!bearerToken) {
+        res.status(401).send("No token found")
+    }
     if (isAuthenticated) {
+        const user = await noteDAO.findUser(isAuthenticated.userId);
+        req.user = user;
         next();
     } else {
         res.status(401).send("You are not authorized to view these notes");
     }
 });
 
-router.post("/", (req, res, next) => {
-    console.log("You posted a new note")
+router.post("/", async (req, res, next) => {
+    const { text } = req.body;
+    const userId = req.user._id;
+    try {
+        const note = await noteDAO.addNote(text, userId)
+        if (note) {
+            res.status(200);
+            res.json(note);
+        } else {
+            res.status(401).send("Note not added");
+        }
+    } catch (err) {
+        res.status(500).send("Oops, server error!")
+    }
 })
 
-router.get("/", (req, res, next) => {
-
+router.get("/", async (req, res, next) => {
+    const userId = req.user._id;
+    try {
+        const notes = await noteDAO.getNotes(userId)
+        if (notes) {
+            res.status(200);
+            res.json(notes);
+        } else {
+            res.status(401).send("No notes found")
+        }
+    } catch (err) {
+        res.status(500).send("Oops, server error!")
+    }
 })
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
     const { id } = req.params;
-    console.log(id);
+    try {
+        const note = await noteDAO.getSingleNote(id)
+        if (note) {
+            res.status(200);
+            res.json(note);
+        } else {
+            res.status(401).send("Could not find the note you were looking for")
+        }
+    } catch (err) {
+        res.status(500).send("Oops, server error!")
+    }
 })
-
-
 
 module.exports = router;
